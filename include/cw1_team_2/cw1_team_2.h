79fe197bc14f60476531d41232663c280cc94a51
Ahmed Adamjee <adamjiahmed@gmail.com>
2022-01-29 19:06:13 +0000

    Added PCL tutorial functions

--- include/cw1_team_2/cw1_team_2.h
+++ include/cw1_team_2/cw1_team_2.h
@@ -42,6 +42,8 @@
 // ROS includes
 #include <std_msgs/String.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Quaternion.h>
@@ -54,9 +56,18 @@
 
 // PCL specific includes
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/centroid.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 // TF specific includes
 #include <tf/transform_broadcaster.h>
@@ -73,150 +84,306 @@
 #include <cw1_team_2/remove_collision.h>
 #include <cw1_team_2/pick.h>
 
+
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointC;
+typedef PointC::Ptr PointCPtr;
+
 /** \brief CW1 Solution.
   *
   * \author Ahmed Adamjee, Abdulbaasit Sanusi, Kennedy Dike
   */
 class Cw1Solution
 {
-public:
+  public:
 
-  /** \brief  Class constructor. 
-    *
-    * \input[in] nh ROS node handle
-    */
-  Cw1Solution(ros::NodeHandle& nh);
+    /** \brief  Class constructor. 
+      *
+      * \input[in] nh ROS node handle
+      */
+    Cw1Solution(ros::NodeHandle& nh);
 
 
-  /** \brief Service callback function for moving the arm. 
-    *
-    * \input[in] request service request message 
-    * \input[in] response service response message
-    *  
-    * \return true if service succeeds
-    */
-  bool 
-  setArmCallback(cw1_team_2::set_arm::Request &request,
-    cw1_team_2::set_arm::Response &response);
-  
-  /** \brief Service callback function for setting the gripper fingers to a specific width. 
-    *
-    * \input[in] request service request message 
-    * \input[in] response service response message
-    *  
-    * \return true if service succeeds
-    */
-  bool 
-  setGripperCallback(cw1_team_2::set_gripper::Request &request,
-    cw1_team_2::set_gripper::Response &response);
-  
-  /** \brief Service callback function for adding collision objects. 
-    *
-    * \input[in] request service request message 
-    * \input[in] response service response message
-    *  
-    * \return true if service succeeds
-    */
-  bool 
-  addCollisionCallback(cw1_team_2::add_collision::Request &request,
-    cw1_team_2::add_collision::Response &response);
-
-  /** \brief Service callback function for removing collision objects. 
-    *
-    * \input[in] request service request message 
-    * \input[in] response service response message
-    *  
-    * \return true if service succeeds
-    */
-  bool 
-  removeCollisionCallback(cw1_team_2::remove_collision::Request &request,
-    cw1_team_2::remove_collision::Response &response);
-
-  /** \brief Service callback function for picking up an object. 
-    *
-    * \input[in] request service request message 
-    * \input[in] response service response message
-    *  
-    * \return true if service succeeds
-    */
-  bool 
-  pickCallback(cw1_team_2::pick::Request &request,
-    cw1_team_2::pick::Response &response);
-
-  /** \brief MoveIt function for moving the move_group to the target position.
-    *
-    * \input[in] target_pose pose to move the arm to
-    *
-    * \return true if moved to target position 
-    */
-  bool 
-  moveArm(geometry_msgs::Pose target_pose);
-
-  /** \brief MoveIt function for moving the gripper fingers to a new position. 
-    *
-    * \input[in] width desired gripper finger width
-    *
-    * \return true if gripper fingers are moved to the new position
-    */
-  bool 
-  moveGripper(float width);
-
-  /** \brief MoveIt function for adding a cuboid collision object in RViz
-    * and the MoveIt planning scene.
-    *
-    * \input[in] object_name name for the new object to be added
-    * \input[in] centre point at which to add the new object
-    * \input[in] dimensions dimensions of the cuboid to add in x,y,z
-    * \input[in] orientation rotation to apply to the cuboid before adding
-    */
-  void
-  addCollisionObject(std::string object_name, geometry_msgs::Point centre, 
-    geometry_msgs::Vector3 dimensions, geometry_msgs::Quaternion orientation);
-
-  /** \brief Remove a collision object from the planning scene.
-    * 
-    * \input[in] object_name for the object to be removed
-    */
-  void 
-  removeCollisionObject(std::string object_name);
-
-  /** \brief Pick an object up with a given position.
-    * 
-    * \input[in] position the xyz coordinates where the gripper converges
-    */
-  bool
-  pick(geometry_msgs::Point position);
+    /** \brief Service callback function for moving the arm. 
+      *
+      * \input[in] request service request message 
+      * \input[in] response service response message
+      *  
+      * \return true if service succeeds
+      */
+    bool 
+    setArmCallback(cw1_team_2::set_arm::Request &request,
+      cw1_team_2::set_arm::Response &response);
     
-  /* Variables */
+    /** \brief Service callback function for setting the gripper fingers to a specific width. 
+      *
+      * \input[in] request service request message 
+      * \input[in] response service response message
+      *  
+      * \return true if service succeeds
+      */
+    bool 
+    setGripperCallback(cw1_team_2::set_gripper::Request &request,
+      cw1_team_2::set_gripper::Response &response);
+    
+    /** \brief Service callback function for adding collision objects. 
+      *
+      * \input[in] request service request message 
+      * \input[in] response service response message
+      *  
+      * \return true if service succeeds
+      */
+    bool 
+    addCollisionCallback(cw1_team_2::add_collision::Request &request,
+      cw1_team_2::add_collision::Response &response);
 
-  /** \brief Define some useful constant values */
-  std::string base_frame_ = "panda_link0";
-  double gripper_open_ = 80e-3;
-  double gripper_closed_ = 0.0;
+    /** \brief Service callback function for removing collision objects. 
+      *
+      * \input[in] request service request message 
+      * \input[in] response service response message
+      *  
+      * \return true if service succeeds
+      */
+    bool 
+    removeCollisionCallback(cw1_team_2::remove_collision::Request &request,
+      cw1_team_2::remove_collision::Response &response);
 
-  /** \brief Parameters to define the pick operation */
-  double z_offset_ = 0.125;
-  double angle_offset_ = 3.14159 / 4.0;
-  double approach_distance_ = 0.1;
+    /** \brief Service callback function for picking up an object. 
+      *
+      * \input[in] request service request message 
+      * \input[in] response service response message
+      *  
+      * \return true if service succeeds
+      */
+    bool 
+    pickCallback(cw1_team_2::pick::Request &request,
+      cw1_team_2::pick::Response &response);
+
+    /** \brief MoveIt function for moving the move_group to the target position.
+      *
+      * \input[in] target_pose pose to move the arm to
+      *
+      * \return true if moved to target position 
+      */
+    bool 
+    moveArm(geometry_msgs::Pose target_pose);
+
+    /** \brief MoveIt function for moving the gripper fingers to a new position. 
+      *
+      * \input[in] width desired gripper finger width
+      *
+      * \return true if gripper fingers are moved to the new position
+      */
+    bool 
+    moveGripper(float width);
+
+    /** \brief MoveIt function for adding a cuboid collision object in RViz
+      * and the MoveIt planning scene.
+      *
+      * \input[in] object_name name for the new object to be added
+      * \input[in] centre point at which to add the new object
+      * \input[in] dimensions dimensions of the cuboid to add in x,y,z
+      * \input[in] orientation rotation to apply to the cuboid before adding
+      */
+    void
+    addCollisionObject(std::string object_name, geometry_msgs::Point centre, 
+      geometry_msgs::Vector3 dimensions, geometry_msgs::Quaternion orientation);
+
+    /** \brief Remove a collision object from the planning scene.
+      * 
+      * \input[in] object_name for the object to be removed
+      */
+    void 
+    removeCollisionObject(std::string object_name);
+
+    /** \brief Pick an object up with a given position.
+      * 
+      * \input[in] position the xyz coordinates where the gripper converges
+      */
+    bool
+    pick(geometry_msgs::Point position);
+
+    // FROM PCL TUTORIAL -->
+    void
+    cloudCallBackOne (const sensor_msgs::PointCloud2ConstPtr& cloud_input_msg);
+    
+    /** \brief Apply Voxel Grid filtering.
+      * 
+      * \input[in] in_cloud_ptr the input PointCloud2 pointer
+      * \input[out] out_cloud_ptr the output PointCloud2 pointer
+      */
+    void
+    applyVX (PointCPtr &in_cloud_ptr,
+             PointCPtr &out_cloud_ptr);
+
+    /** \brief Apply Pass Through filtering.
+      * 
+      * \input[in] in_cloud_ptr the input PointCloud2 pointer
+      * \input[out] out_cloud_ptr the output PointCloud2 pointer
+      */
+    void
+    applyPT (PointCPtr &in_cloud_ptr,
+             PointCPtr &out_cloud_ptr);
+    
+    /** \brief Normal estimation.
+      * 
+      * \input[in] in_cloud_ptr the input PointCloud2 pointer
+      */
+    void
+    findNormals (PointCPtr &in_cloud_ptr);
+    
+    /** \brief Segment Plane from point cloud.
+      * 
+      * \input[in] in_cloud_ptr the input PointCloud2 pointer
+      */
+    void
+    segPlane (PointCPtr &in_cloud_ptr);
+    
+    /** \brief Segment Cylinder from point cloud.
+      * 
+      * \input[in] in_cloud_ptr the input PointCloud2 pointer
+      */
+    void
+    segCylind (PointCPtr &in_cloud_ptr);
+    
+    
+    /** \brief Find the Pose of Cylinder.
+      * 
+      * \input[in] in_cloud_ptr the input PointCloud2 pointer
+      */
+    void
+    findCylPose (PointCPtr &in_cloud_ptr);
+    
+    /** \brief Point Cloud publisher.
+      * 
+      *  \input pc_pub ROS publisher
+      *  \input pc point cloud to be published
+      */
+    void
+    pubFilteredPCMsg (ros::Publisher &pc_pub, PointC &pc);
+    
+    /** \brief Publish the cylinder point.
+      * 
+      *  \input[in] cyl_pt_msg Cylinder's geometry point
+      *  
+      *  \output true if three numbers are added
+      */
+    void
+    publishPose (geometry_msgs::PointStamped &cyl_pt_msg);
+    
+
+
+      
+    /* Variables */
+
+    /** \brief Define some useful constant values */
+    std::string base_frame_ = "panda_link0";
+    double gripper_open_ = 80e-3;
+    double gripper_closed_ = 0.0;
+
+    /** \brief Parameters to define the pick operation */
+    double z_offset_ = 0.125;
+    double angle_offset_ = 3.14159 / 4.0;
+    double approach_distance_ = 0.1;
+    
+    /** \brief Node handle. */
+    ros::NodeHandle g_nh;
+
+    /** \brief  service servers for advertising ROS services  */
+    ros::ServiceServer set_arm_srv_;
+    ros::ServiceServer set_gripper_srv_;
+    ros::ServiceServer add_collision_srv_;
+    ros::ServiceServer remove_collision_srv_;
+    ros::ServiceServer pick_srv_;
+
+    /** \brief MoveIt interface to move groups to seperate the arm and the gripper,
+      * these are defined in urdf. */
+    moveit::planning_interface::MoveGroupInterface arm_group_{"panda_arm"};
+    moveit::planning_interface::MoveGroupInterface hand_group_{"hand"};
+
+    /** \brief MoveIt interface to interact with the moveit planning scene 
+      * (eg collision objects). */
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+
+
+    /** \brief The input point cloud frame id. */
+    std::string g_input_pc_frame_id_;
+
+    /** \brief ROS publishers. */
+    ros::Publisher g_pub_cloud;
+    
+    /** \brief ROS geometry message point. */
+    geometry_msgs::PointStamped g_cyl_pt_msg;
+    
+    /** \brief ROS pose publishers. */
+    ros::Publisher g_pub_pose;
+    
+    /** \brief Voxel Grid filter's leaf size. */
+    double g_vg_leaf_sz;
+    
+    /** \brief Point Cloud (input) pointer. */
+    PointCPtr g_cloud_ptr;
+    
+    /** \brief Point Cloud (filtered) pointer. */
+    PointCPtr g_cloud_filtered, g_cloud_filtered2;
+    
+    /** \brief Point Cloud (filtered) sensros_msg for publ. */
+    sensor_msgs::PointCloud2 g_cloud_filtered_msg;
+    
+    /** \brief Point Cloud (input). */
+    pcl::PCLPointCloud2 g_pcl_pc;
+    
+    /** \brief Voxel Grid filter. */
+    pcl::VoxelGrid<PointT> g_vx;
+    
+    /** \brief Pass Through filter. */
+    pcl::PassThrough<PointT> g_pt;
+    
+    /** \brief Pass Through min and max threshold sizes. */
+    double g_pt_thrs_min, g_pt_thrs_max;
+    
+    /** \brief KDTree for nearest neighborhood search. */
+    pcl::search::KdTree<PointT>::Ptr g_tree_ptr;
+    
+    /** \brief Normal estimation. */
+    pcl::NormalEstimation<PointT, pcl::Normal> g_ne;
+    
+    /** \brief Cloud of normals. */
+    pcl::PointCloud<pcl::Normal>::Ptr g_cloud_normals, g_cloud_normals2;
+    
+    /** \brief Nearest neighborhooh size for normal estimation. */
+    double g_k_nn;
+    
+    /** \brief SAC segmentation. */
+    pcl::SACSegmentationFromNormals<PointT, pcl::Normal> g_seg; 
+    
+    /** \brief Extract point cloud indices. */
+    pcl::ExtractIndices<PointT> g_extract_pc;
   
-  /** \brief Node handle. */
-  ros::NodeHandle g_nh;
+    /** \brief Extract point cloud normal indices. */
+    pcl::ExtractIndices<pcl::Normal> g_extract_normals;
+    
+    /** \brief Point indices for plane. */
+    pcl::PointIndices::Ptr g_inliers_plane;
+      
+    /** \brief Point indices for cylinder. */
+    pcl::PointIndices::Ptr g_inliers_cylinder;
+    
+    /** \brief Model coefficients for the plane segmentation. */
+    pcl::ModelCoefficients::Ptr g_coeff_plane;
+    
+    /** \brief Model coefficients for the culinder segmentation. */
+    pcl::ModelCoefficients::Ptr g_coeff_cylinder;
+    
+    /** \brief Point cloud to hold plane and cylinder points. */
+    PointCPtr g_cloud_plane, g_cloud_cylinder;
+    
+    /** \brief cw1Q1: TF listener definition. */
+    tf::TransformListener g_listener_;
+    
 
-  /** \brief  service servers for advertising ROS services  */
-  ros::ServiceServer set_arm_srv_;
-  ros::ServiceServer set_gripper_srv_;
-  ros::ServiceServer add_collision_srv_;
-  ros::ServiceServer remove_collision_srv_;
-  ros::ServiceServer pick_srv_;
-
-  /** \brief MoveIt interface to move groups to seperate the arm and the gripper,
-    * these are defined in urdf. */
-  moveit::planning_interface::MoveGroupInterface arm_group_{"panda_arm"};
-  moveit::planning_interface::MoveGroupInterface hand_group_{"hand"};
-
-  /** \brief MoveIt interface to interact with the moveit planning scene 
-    * (eg collision objects). */
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
-
+  protected:
+    /** \brief Debug mode. */
+    bool debug_;
 };
 #endif
--- src/cw1_team_2.cpp
+++ src/cw1_team_2.cpp
@@ -34,10 +34,37 @@
 
 #include <cw1_team_2/cw1_team_2.h>
 
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointC;
+typedef PointC::Ptr PointCPtr;
+
 ////////////////////////////////////////////////////////////////////////////////
-Cw1Solution::Cw1Solution (ros::NodeHandle &nh)
+Cw1Solution::Cw1Solution (ros::NodeHandle &nh):
+  g_cloud_ptr (new PointC), // input point cloud
+  g_cloud_filtered (new PointC), // filtered point cloud
+  g_cloud_filtered2 (new PointC), // filtered point cloud
+  g_cloud_plane (new PointC), // plane point cloud
+  g_cloud_cylinder (new PointC), // cylinder point cloud
+  g_tree_ptr (new pcl::search::KdTree<PointT> ()), // KdTree
+  g_cloud_normals (new pcl::PointCloud<pcl::Normal>), // segmentation
+  g_cloud_normals2 (new pcl::PointCloud<pcl::Normal>), // segmentation
+  g_inliers_plane (new pcl::PointIndices), // plane seg
+  g_inliers_cylinder (new pcl::PointIndices), // cylidenr seg
+  g_coeff_plane (new pcl::ModelCoefficients), // plane coeff
+  g_coeff_cylinder (new pcl::ModelCoefficients), // cylinder coeff
+  debug_ (false)
 {
   g_nh = nh;
+
+  // Define the publishers
+  g_pub_cloud = nh.advertise<sensor_msgs::PointCloud2> ("filtered_cloud", 1, true);
+  g_pub_pose = nh.advertise<geometry_msgs::PointStamped> ("cyld_pt", 1, true);
+  
+  // Define public variables
+  g_vg_leaf_sz = 0.01; // VoxelGrid leaf size: Better in a config file
+  g_pt_thrs_min = 0.0; // PassThrough min thres: Better in a config file
+  g_pt_thrs_max = 0.7; // PassThrough max thres: Better in a config file
+  g_k_nn = 50; // Normals nn size: Better in a config file
 
   // namespace for our ROS services, they will appear as "/namespace/srv_name"
   std::string service_ns = "/cw1_team_2";
@@ -344,4 +371,197 @@ Cw1Solution::pick(geometry_msgs::Point position)
   return true;
 }
 
-///////////////////////////////////////////////////////////////////////////////
+////////////FROM PCL TUTORIAL//////////////////////////////
+
+void
+Cw1Solution::cloudCallBackOne
+  (const sensor_msgs::PointCloud2ConstPtr &cloud_input_msg)
+{
+  // Extract inout point cloud info
+  g_input_pc_frame_id_ = cloud_input_msg->header.frame_id;
+    
+  // Convert to PCL data type
+  pcl_conversions::toPCL (*cloud_input_msg, g_pcl_pc);
+  pcl::fromPCLPointCloud2 (g_pcl_pc, *g_cloud_ptr);
+
+  // Perform the filtering
+  applyVX (g_cloud_ptr, g_cloud_filtered);
+  //applyPT (g_cloud_ptr, g_cloud_filtered);
+  
+  // Segment plane and cylinder
+  //findNormals (g_cloud_filtered);
+  //segPlane (g_cloud_filtered);
+  //segCylind (g_cloud_filtered);
+  //findCylPose (g_cloud_cylinder);
+    
+  // Publish the data
+  //ROS_INFO ("Publishing Filtered Cloud 2");
+  pubFilteredPCMsg (g_pub_cloud, *g_cloud_filtered);
+  //pubFilteredPCMsg (g_pub_cloud, *g_cloud_cylinder);
+  
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::applyVX (PointCPtr &in_cloud_ptr,
+                      PointCPtr &out_cloud_ptr)
+{
+  g_vx.setInputCloud (in_cloud_ptr);
+  g_vx.setLeafSize (g_vg_leaf_sz, g_vg_leaf_sz, g_vg_leaf_sz);
+  g_vx.filter (*out_cloud_ptr);
+  
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::applyPT (PointCPtr &in_cloud_ptr,
+                      PointCPtr &out_cloud_ptr)
+{
+  g_pt.setInputCloud (in_cloud_ptr);
+  g_pt.setFilterFieldName ("x");
+  g_pt.setFilterLimits (g_pt_thrs_min, g_pt_thrs_max);
+  g_pt.filter (*out_cloud_ptr);
+  
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::findNormals (PointCPtr &in_cloud_ptr)
+{
+  // Estimate point normals
+  g_ne.setInputCloud (in_cloud_ptr);
+  g_ne.setSearchMethod (g_tree_ptr);
+  g_ne.setKSearch (g_k_nn);
+  g_ne.compute (*g_cloud_normals);
+  
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::segPlane (PointCPtr &in_cloud_ptr)
+{
+  // Create the segmentation object for the planar model
+  // and set all the params
+  g_seg.setOptimizeCoefficients (true);
+  g_seg.setModelType (pcl::SACMODEL_NORMAL_PLANE);
+  g_seg.setNormalDistanceWeight (0.1); //bad style
+  g_seg.setMethodType (pcl::SAC_RANSAC);
+  g_seg.setMaxIterations (100); //bad style
+  g_seg.setDistanceThreshold (0.03); //bad style
+  g_seg.setInputCloud (in_cloud_ptr);
+  g_seg.setInputNormals (g_cloud_normals);
+  // Obtain the plane inliers and coefficients
+  g_seg.segment (*g_inliers_plane, *g_coeff_plane);
+  
+  // Extract the planar inliers from the input cloud
+  g_extract_pc.setInputCloud (in_cloud_ptr);
+  g_extract_pc.setIndices (g_inliers_plane);
+  g_extract_pc.setNegative (false);
+  
+  // Write the planar inliers to disk
+  g_extract_pc.filter (*g_cloud_plane);
+  
+  // Remove the planar inliers, extract the rest
+  g_extract_pc.setNegative (true);
+  g_extract_pc.filter (*g_cloud_filtered2);
+  g_extract_normals.setNegative (true);
+  g_extract_normals.setInputCloud (g_cloud_normals);
+  g_extract_normals.setIndices (g_inliers_plane);
+  g_extract_normals.filter (*g_cloud_normals2);
+
+  //ROS_INFO_STREAM ("Plane coefficients: " << *g_coeff_plane);
+  ROS_INFO_STREAM ("PointCloud representing the planar component: "
+                   << g_cloud_plane->size ()
+                   << " data points.");
+}
+    
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::segCylind (PointCPtr &in_cloud_ptr)
+{
+  // Create the segmentation object for cylinder segmentation
+  // and set all the parameters
+  g_seg.setOptimizeCoefficients (true);
+  g_seg.setModelType (pcl::SACMODEL_CYLINDER);
+  g_seg.setMethodType (pcl::SAC_RANSAC);
+  g_seg.setNormalDistanceWeight (0.1); //bad style
+  g_seg.setMaxIterations (10000); //bad style
+  g_seg.setDistanceThreshold (0.05); //bad style
+  g_seg.setRadiusLimits (0, 0.1); //bad style
+  g_seg.setInputCloud (g_cloud_filtered2);
+  g_seg.setInputNormals (g_cloud_normals2);
+
+  // Obtain the cylinder inliers and coefficients
+  g_seg.segment (*g_inliers_cylinder, *g_coeff_cylinder);
+  
+  // Write the cylinder inliers to disk
+  g_extract_pc.setInputCloud (g_cloud_filtered2);
+  g_extract_pc.setIndices (g_inliers_cylinder);
+  g_extract_pc.setNegative (false);
+  g_extract_pc.filter (*g_cloud_cylinder);
+  
+  ROS_INFO_STREAM ("PointCloud representing the cylinder component: "
+                   << g_cloud_cylinder->size ()
+                   << " data points.");
+  
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::findCylPose (PointCPtr &in_cloud_ptr)
+{
+  Eigen::Vector4f centroid_in;
+  pcl::compute3DCentroid(*in_cloud_ptr, centroid_in);
+  
+  g_cyl_pt_msg.header.frame_id = g_input_pc_frame_id_;
+  g_cyl_pt_msg.header.stamp = ros::Time (0);
+  g_cyl_pt_msg.point.x = centroid_in[0];
+  g_cyl_pt_msg.point.y = centroid_in[1];
+  g_cyl_pt_msg.point.z = centroid_in[2];
+  
+  // Transform the point to new frame
+  geometry_msgs::PointStamped g_cyl_pt_msg_out;
+  try
+  {
+    g_listener_.transformPoint ("panda_link0",  // bad styling
+                                g_cyl_pt_msg,
+                                g_cyl_pt_msg_out);
+    //ROS_INFO ("trying transform...");
+  }
+  catch (tf::TransformException& ex)
+  {
+    ROS_ERROR ("Received a trasnformation exception: %s", ex.what());
+  }
+  
+  publishPose (g_cyl_pt_msg_out);
+  
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::pubFilteredPCMsg (ros::Publisher &pc_pub,
+                               PointC &pc)
+{
+  // Publish the data
+  pcl::toROSMsg(pc, g_cloud_filtered_msg);
+  pc_pub.publish (g_cloud_filtered_msg);
+  
+  return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+Cw1Solution::publishPose (geometry_msgs::PointStamped &cyl_pt_msg)
+{
+  // Create and publish the cylinder pose (ignore orientation)
+
+  g_pub_pose.publish (cyl_pt_msg);
+  
+  return;
+}
